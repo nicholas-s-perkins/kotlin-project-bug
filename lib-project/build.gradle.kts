80c2a8d7ff2e5900da3a4a0bdc833dee270eca79
Nicholas Perkins <nsperkins@spscommerce.com>
2019-07-26 14:50:44 -0500

    removed some stuff

--- lib-project/build.gradle.kts
+++ lib-project/build.gradle.kts
@@ -1,8 +1,7 @@
 import com.nsperkins.kotlin.Versions
 
 plugins {
-    `java-library`
-    `maven-publish`
+    java
     kotlin("jvm") version "1.3.41"
 }
 
@@ -15,31 +14,31 @@ version = "1.0.0-SNAPSHOT"
 
 dependencies {
     implementation(kotlin("stdlib"))
-    api(group = "org.apache.commons", name = "commons-lang3", version = Versions.commonsLang)
+    implementation(group = "org.apache.commons", name = "commons-lang3", version = Versions.commonsLang)
 
     testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = Versions.junit)
 }
 
-val sourcesJar by tasks.registering(Jar::class){
-    from(sourceSets.main.get().allSource)
-    archiveClassifier.set("sources")
-}
-
-val javadocJar by tasks.registering(Jar::class){
-    from(tasks.javadoc)
-    archiveClassifier.set("javadoc")
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-            artifact(sourcesJar.get())
-            artifact(javadocJar.get())
-            pom {
-                name.set("my-lib")
-                description.set("a test lib")
-            }
-        }
-    }
-}
+//val sourcesJar by tasks.registering(Jar::class){
+//    from(sourceSets.main.get().allSource)
+//    archiveClassifier.set("sources")
+//}
+//
+//val javadocJar by tasks.registering(Jar::class){
+//    from(tasks.javadoc)
+//    archiveClassifier.set("javadoc")
+//}
+//
+//publishing {
+//    publications {
+//        create<MavenPublication>("mavenJava") {
+//            from(components["java"])
+//            artifact(sourcesJar.get())
+//            artifact(javadocJar.get())
+//            pom {
+//                name.set("my-lib")
+//                description.set("a test lib")
+//            }
+//        }
+//    }
+//}
